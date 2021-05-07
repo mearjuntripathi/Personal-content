76f60a1532642b2d1729f04729c83d7f21bd7005
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-05-07 15:36:34 +0530

    Create Compare the Triplets.cpp
    https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true

---
+++ Compare the Triplets.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a0,a1,a2;   cin >> a0 >> a1 >> a2;
+    int b0,b1,b2;   cin >> b0 >> b1 >> b2;
+    int a=0,b=0;
+    
+    if(a0>b0)
+        a++;
+    if(a0<b0)
+        b++;
+    if(a1>b1)
+        a++;
+    if(a1<b1)
+        b++;
+    if(a2>b2)
+        a++;
+    if(a2<b2)
+        b++;
+    cout<<a<<" "<<b;
+    return 0;
+    
+}
