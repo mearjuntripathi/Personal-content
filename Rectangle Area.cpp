00d42f3a07317a05a2797b590c8446aa9053fdf7
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-05-08 00:45:36 +0530

    Update Rectangle Area.cpp
    https://www.hackerrank.com/challenges/rectangle-area/problem?isFullScreen=true

--- Rectangle Area.cpp
+++ Rectangle Area.cpp
@@ -1,4 +1,7 @@
+
+
 #include <iostream>
+
 using namespace std;
     class Rectangle 
     {
@@ -24,4 +27,31 @@ using namespace std;
         }
     };
     
-  
+    
+    
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+    
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+    
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+    
+    /*
+     * Print the area
+     */
+    r_area.display();
+    
+    return 0;
+}
+
