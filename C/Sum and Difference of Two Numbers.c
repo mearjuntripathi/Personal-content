5a26c1500f33a3f9b6d1ffa7b24edc965005fdb7
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-06 00:09:32 +0530

    Create Sum and Difference of Two Numbers.c
    https://www.hackerrank.com/challenges/sum-numbers-c/problem

---
+++ C/Sum and Difference of Two Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+   int m,n;
+   scanf("%d%d",&m,&n);
+   float a,b;
+   scanf("%f%f",&a,&b);
+   printf("%d",m+n);
+   printf(" %d",m-n); 
+   printf("\n%0.1f",a+b);
+   printf(" %0.1f",a-b);  
+
+    return 0;
+}
