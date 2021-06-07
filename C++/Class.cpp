cb92dee900bd4557086541f21fa9085640f80f18
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-07 11:55:36 +0530

    Create Class.cpp
    https://www.hackerrank.com/challenges/c-tutorial-class/problem?isFullScreen=true

---
+++ C++/Class.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age,standard;
+    string first_name,last_name;
+    cin>>age>>first_name>>last_name>>standard;
+    
+    cout<<age<<endl<<last_name<<", "<<first_name<<endl<<standard<<endl;
+    
+    cout<<endl<<age<<","<<first_name<<","<<last_name<<","<<standard<<endl;
+    return 0;
+}
