b47fa5edff3843b5defac552dd8f870674367c02
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-18 23:05:36 +0530

    Create Pointers in C.c
    https://www.hackerrank.com/challenges/pointer-in-c/problem

---
+++ C/Pointers in C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+   printf("%d",a+b);
+   printf("\n%d",abs(a-b)); 
+    return 0;
+}
