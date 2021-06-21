ac42fa9cd353a721c49e96d1739c43ff55a3bd19
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-21 19:08:52 +0530

    Create Arrays Introduction.cpp
    https://www.hackerrank.com/challenges/arrays-introduction/problem

---
+++ C++/Arrays Introduction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+      
+    for(int i=n; i>=1; i--)
+    
+        {   
+        cout<<arr[i-1]<<" ";
+        }
+    
+return 0;    
+    
+}
