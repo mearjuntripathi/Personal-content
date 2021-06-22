c318b90f99b1b0165182e34ade50dde8039633e3
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-22 16:41:22 +0530

    Create Basic Data Types.c
    https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem

---
+++ C++/Basic Data Types.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+   int a;
+   long b;
+   char c;
+   float d;
+   double e;
+   cin>>a>>b>>c>>d>>e;
+   cout<<a<<endl<<b<<endl<<c<<endl;
+    cout << std::fixed << std::setprecision(3) << d << '\n';
+    cout << std::fixed << std::setprecision(9) << e << '\n';
+ 
+    return 0;
+}
