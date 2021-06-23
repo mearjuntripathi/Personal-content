039c61f3d4fd9536ab0e22f9067976c25f972e8a
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-06-23 17:43:12 +0530

    Create Functions.cpp
    https://www.hackerrank.com/challenges/c-tutorial-functions/problem

---
+++ C++/Functions.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() 
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if((a>b)&&(a>c)&&(a>d))
+    {
+        cout<<a;
+    }
+    else if((b>a)&&(b>c)&&(b>d))
+    {
+        cout<<b;
+    }
+   else if((c>a)&&(c>b)&&(c>d))
+    {
+        cout<<c;
+    }
+    else
+    {
+        cout<<d;
+    }
+    return 0;
+}
+
+
